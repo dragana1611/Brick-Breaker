b3e966d2f456369ef232086272012a5d4ace9176
dragana1611 <dragana16111@gmail.com>
2022-04-09 21:30:37 +0200

    updated

--- script.js
+++ script.js
@@ -1,163 +1,192 @@
-var Platno = document.getElementById("Platno");
-var crtaj = Platno.getContext("2d");
-var poluprecnikLopte = 10;
-var x = Platno.width / 2;
-var y = Platno.height - 30;
-var dx = 2;
-var dy = -2;
-var visinaPrecage = 10;
-var sirinaPrecage = 80;
-var precagaXpozicija = (Platno.width - sirinaPrecage) / 2;
-var idiDesno = false;
-var idiLevo = false;
-var cigleBruRedu = 10;
-var cigleBruKoloni = 6;
-var sirinaCigle = 75;
-var visinaCigle = 20;
-var luftCigle = 10;
-var luftCigliGore = 30;
-var luftCigliLevo = 30;
-var bodovi = 0; //*novo
-var zivoti = 2; //*novo	 
+const Platno = document.getElementById("Platno");
+const btnStart = document.getElementById("start");
+const btnRestart = document.getElementById("restart");
 
-var cigle = [];
-for (var c = 0; c < cigleBruKoloni; c++) {
-    cigle[c] = [];
-    for (var r = 0; r < cigleBruRedu; r++) {
-        cigle[c][r] = { x: 0, y: 0, status: 1 };
-    }
+const crtaj = Platno.getContext("2d");
+let poluprecnikLopte = 10;
+let x = Platno.width / 2;
+let y = Platno.height - 30;
+let dx = 2;
+let dy = -2;
+let visinaPrecage = 20;
+let sirinaPrecage = 100;
+let precagaXpozicija = (Platno.width - sirinaPrecage) / 2;
+let idiDesno = false;
+let idiLevo = false;
+let cigleBruRedu = 10; // broj kolona
+let cigleBruKoloni = 6; // broj redova
+let sirinaCigle = 75;
+let visinaCigle = 20;
+let luftCigle = 10;
+let luftCigliGore = 30;
+let luftCigliLevo = 30;
+let bodovi = 0;
+let zivoti = 3;
+let interval = null;
+
+let cigle = [];
+for (let c = 0; c < cigleBruKoloni; c++) {
+  cigle[c] = [];
+  for (let r = 0; r < cigleBruRedu; r++) {
+    cigle[c][r] = { x: 0, y: 0, status: 1 };
+  }
 }
 
+window.addEventListener("load", iscrtaj);
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+btnStart.addEventListener("click", startGame);
+btnRestart.addEventListener("click", restartGame);
+
+function startGame() {
+  if (!interval)
+    //prevents acceleration of setInterval
+    interval = setInterval(iscrtaj, 10);
+}
+
+function restartGame() {
+  location.reload(true);
+}
 
 function keyDownHandler(e) {
-    if (e.code == "ArrowRight") {
-        idiDesno = true;
-    }
-    else if (e.code == 'ArrowLeft') {
-        idiLevo = true;
-    }
+  if (e.code == "ArrowRight") {
+    idiDesno = true;
+  } else if (e.code == "ArrowLeft") {
+    idiLevo = true;
+  }
 }
 function keyUpHandler(e) {
-    if (e.code == "ArrowRight") {
-        idiDesno = false;
-    }
-    else if (e.code == 'ArrowLeft') {
-        idiLevo = false;
-    }
+  if (e.code == "ArrowRight") {
+    idiDesno = false;
+  } else if (e.code == "ArrowLeft") {
+    idiLevo = false;
+  }
 }
 function proveraSudara() {
-    for (var c = 0; c < cigleBruKoloni; c++) {
-        for (var r = 0; r < cigleBruRedu; r++) {
-            var b = cigle[c][r];
-            if (b.status == 1) {
-                if (x > b.x && x < b.x + sirinaCigle && y > b.y && y < b.y + visinaCigle) {
-                    dy = -dy;
-                    b.status = 0;
-                    //*novo
-                    bodovi++;
-                    if (bodovi == cigleBruRedu * cigleBruKoloni) {
-                        alert("Pobeda!");
-                        document.location.reload();
-                    }
-                    //*novo						
-                }
-            }
+  for (let c = 0; c < cigleBruKoloni; c++) {
+    for (let r = 0; r < cigleBruRedu; r++) {
+      let b = cigle[c][r];
+      if (b.status == 1) {
+        if (
+          x < b.x + sirinaCigle &&
+          x + 2 * poluprecnikLopte > b.x &&
+          y < b.y + visinaCigle &&
+          y + 2 * poluprecnikLopte > b.y
+        ) {
+          dy = -dy;
+          b.status = 0;
+
+          bodovi++;
+          if (bodovi == cigleBruRedu * cigleBruKoloni) {
+            alert("Pobeda!");
+            location.reload(true);
+          }
         }
+      }
     }
+  }
 }
 
+console.log(precagaXpozicija);
+
 function iscrtajLoptu() {
-    crtaj.beginPath();
-    crtaj.arc(x, y, poluprecnikLopte, 0, Math.PI * 2);
-    crtaj.fillStyle = "#28c953";
-    crtaj.fill();
-    crtaj.closePath();
+  crtaj.beginPath();
+  crtaj.arc(x, y, poluprecnikLopte, 0, Math.PI * 2);
+  crtaj.fillStyle = "#7BFD60";
+  crtaj.fill();
+  crtaj.closePath();
 }
 function iscrtajPrecagu() {
-    crtaj.beginPath();
-    crtaj.rect(precagaXpozicija, Platno.height - visinaPrecage, sirinaPrecage, visinaPrecage);
-    crtaj.fillStyle = "#0ab8f2";
-    crtaj.fill();
-    crtaj.closePath();
+  const grad = crtaj.createLinearGradient(400, 620, 500, 640);
+  grad.addColorStop(0, "#003297");
+  grad.addColorStop(1, "#0070bb");
+  crtaj.beginPath();
+  crtaj.fillStyle = grad;
+  crtaj.fillRect(
+    precagaXpozicija,
+    Platno.height - visinaPrecage,
+    sirinaPrecage,
+    visinaPrecage
+  );
+  crtaj.closePath();
 }
+
 function iscrtajCigle() {
-    for (var c = 0; c < cigleBruKoloni; c++) {
-        for (var r = 0; r < cigleBruRedu; r++) {
-            if (cigle[c][r].status == 1) {
-                var ciglaXpoz = (r * (sirinaCigle + luftCigle)) + luftCigliLevo;
-                var ciglaYpoz = (c * (visinaCigle + luftCigle)) + luftCigliGore;
-                cigle[c][r].x = ciglaXpoz;
-                cigle[c][r].y = ciglaYpoz;
-                crtaj.beginPath();
-                crtaj.rect(ciglaXpoz, ciglaYpoz, sirinaCigle, visinaCigle);
-                crtaj.fillStyle = "#de430b";
-                crtaj.fill();
-                crtaj.closePath();
-            }
-        }
+  for (let c = 0; c < cigleBruKoloni; c++) {
+    for (let r = 0; r < cigleBruRedu; r++) {
+      if (cigle[c][r].status == 1) {
+        let ciglaXpoz = r * (sirinaCigle + luftCigle) + luftCigliLevo;
+        let ciglaYpoz = c * (visinaCigle + luftCigle) + luftCigliGore;
+
+        cigle[c][r].x = ciglaXpoz;
+        cigle[c][r].y = ciglaYpoz;
+
+        const grad = crtaj.createLinearGradient(0, 0, 900, 300);
+        grad.addColorStop(0, "rgba(112, 234, 234, 0.8)");
+        grad.addColorStop(1, "rgba(218, 253, 253, 0.8)");
+        crtaj.beginPath();
+        crtaj.fillStyle = grad;
+        crtaj.fillRect(ciglaXpoz, ciglaYpoz, sirinaCigle, visinaCigle);
+        crtaj.closePath();
+      }
     }
+  }
 }
-//*novo
+
 function iscrtajBodove() {
-    crtaj.font = "16px Georgia";
-    crtaj.fillStyle = "#f8ff36";
-    crtaj.fillText("Bodovi: " + bodovi, 8, 20);
+  crtaj.font = "25px Georgia";
+  crtaj.fillStyle = "#F5EDB3";
+  crtaj.fillText("Score: " + bodovi, 30, 20);
 }
 
 function iscrtajZivote() {
-    crtaj.font = "16px Georgia";
-    crtaj.fillStyle = "#ff0000";
-    crtaj.fillText("Zivoti: " + zivoti, Platno.width - 65, 20);
+  crtaj.font = "25px Georgia";
+  crtaj.fillStyle = "#E84130";
+  crtaj.fillText("Lives: " + zivoti + " " + "❤️", Platno.width - 150, 20);
 }
-//*novo
+
 function iscrtaj() {
-    crtaj.clearRect(0, 0, Platno.width, Platno.height);
-    iscrtajCigle();
-    iscrtajLoptu();
-    iscrtajPrecagu();
-    iscrtajBodove(); //*novo
-    iscrtajZivote(); //*novo
-    proveraSudara();
+  crtaj.clearRect(0, 0, Platno.width, Platno.height);
+  iscrtajCigle();
+  iscrtajLoptu();
+  iscrtajPrecagu();
+  iscrtajBodove();
+  iscrtajZivote();
+  proveraSudara();
 
-    if (x + dx > Platno.width - poluprecnikLopte || x + dx < poluprecnikLopte) {
-        dx = -dx;
+  if (x + dx > Platno.width - poluprecnikLopte || x + dx < poluprecnikLopte) {
+    dx = -dx;
+  }
+  if (y + dy < 2 * poluprecnikLopte) {
+    dy = -dy;
+  } else if (y + dy > Platno.height - 2 * poluprecnikLopte) {
+    if (
+      x + 2 * poluprecnikLopte > precagaXpozicija &&
+      x < precagaXpozicija + sirinaPrecage
+    ) {
+      dy = -dy;
+    } else {
+      zivoti--;
+      if (!zivoti) {
+        ///
+        alert("Kraj Igre");
+        location.reload(true);
+      } else {
+        x = Platno.width / 2;
+        y = Platno.height - 30;
+        dx = 2;
+        dy = -2;
+        precagaXpozicija = (Platno.width - sirinaPrecage) / 2;
+      }
     }
-    if (y + dy < poluprecnikLopte) {
-        dy = -dy;
-    }
-    else if (y + dy > Platno.height - poluprecnikLopte) {
-        if (x > precagaXpozicija && x < precagaXpozicija + sirinaPrecage) {
-            dy = -dy;
-        }
-        else {
-            //*novo
-            zivoti--;
-            if (!zivoti) {
-                alert("Kraj Igre");
-                document.location.reload();
-            }
-            else {
-                x = Platno.width / 2;
-                y = Platno.height - 30;
-                dx = 2;
-                dy = -2;
-                precagaXpozicija = (Platno.width - sirinaPrecage) / 2;
-            }
-            //*novo
-        }
-    }
+  }
 
-    if (idiDesno && precagaXpozicija < Platno.width - sirinaPrecage) {
-        precagaXpozicija += 5;
-    }
-    else if (idiLevo && precagaXpozicija > 0) {
-        precagaXpozicija -= 5;
-    }
+  if (idiDesno && precagaXpozicija < Platno.width - sirinaPrecage) {
+    precagaXpozicija += 5;
+  } else if (idiLevo && precagaXpozicija > 0) {
+    precagaXpozicija -= 5;
+  }
 
-    x += dx;
-    y += dy;
+  x += dx;
+  y += dy;
 }
-
-setInterval(iscrtaj, 10);
